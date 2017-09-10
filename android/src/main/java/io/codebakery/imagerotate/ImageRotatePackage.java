59352a949fee0e9808c4d12ffb30d8ac8789b08b
brendito <bren2030@gmail.com>
2017-09-10 09:11:02 -0400

    remove deprecated override in RN 0.47

--- android/src/main/java/io/codebakery/imagerotate/ImageRotatePackage.java
+++ android/src/main/java/io/codebakery/imagerotate/ImageRotatePackage.java
@@ -18,7 +18,8 @@ public class ImageRotatePackage implements ReactPackage {
             new ImageRotateModule(reactContext));
     }
 
-    @Override
+    // Deprecated RN 0.47
+    // @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
